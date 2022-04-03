c40dab1bd20c1ba57bb4e588ef33e6fda3d3b1e8
jj81267628 <53717265+jj81267628@users.noreply.github.com>
2022-04-03 15:41:53 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const {
+  MongoClient
+} = require("mongodb");
+
+// Connection URI
+const uri = "mongodb://localhost:27017";
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useUnifiedTopology: true
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    const database = client.db("fruitsDB")
+
+    // Establish and verify connection
+    await database.command({
+      ping: 1
+    });
+    console.log("Connected successfully to server");
+
+    const docs = [{
+        name: "cake",
+        healthy: false
+      },
+      {
+        name: "lettuce",
+        healthy: true
+      },
+      {
+        name: "donut",
+        healthy: false
+      }
+    ];
+    insertDocuments(database, docs);
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+async function insertDocuments(database, docs) {
+
+  const fruitsCollection = database.collection("fruits")
+
+  // this option prevents additional documents from being inserted if one fails
+  const options = {
+    ordered: true
+  };
+  const result = await fruitsCollection.insertMany(docs, options);
+  console.log(`${result.insertedCount} documents were inserted`);
+
+}
+
+run().catch(console.dir);
